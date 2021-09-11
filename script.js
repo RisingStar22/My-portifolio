ffcf30bd816f7ff1c89a004ae30df35dab5afd25
Edford Jongwe <49975587+RisingStar22@users.noreply.github.com>
2021-09-11 14:41:33 +0800

    changed mobile menu

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector("#hamburger");
 const mobileMenu = document.querySelector("#mobile-links");
 hamburger.addEventListener("click", () => {
   if (hamburger.classList.contains("open")) {
+    document.body.style.overflow = "scroll";
     hamburger.classList.remove("open");
     mobileMenu.classList.remove("active");
   } else {
+    document.body.style.overflow = "hidden";
     hamburger.classList.add("open");
     mobileMenu.classList.add("active");
   }
